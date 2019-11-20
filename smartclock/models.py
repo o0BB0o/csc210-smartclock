70863c678c289b7ea6441e2c034fd47fbc33a731
Islomzhan Akhmedov <islomzhan@gmail.com>
2019-11-20 17:51:45 -0500

    the timesheet table works >> SQL

--- smartclock/models.py
+++ smartclock/models.py
@@ -1,6 +1,7 @@
 from smartclock import db, login_manager
 from flask_login import UserMixin
-from smartclock.functions import tableDoesNotExist
+from smartclock.functions import tableDoesNotExist, hash_password
+from datetime import datetime
 
 """
 Learn more here:
@@ -16,14 +17,72 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 class User(db.Model, UserMixin):
+
+    # required
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(120), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.Text, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    is_supervisor = db.Column(db.Boolean, default=False)
+    hourly_rate = db.Column(db.Float, default=12.75)
+
+    # optional
+    #
+    # first_name = db.Column(db.String(120), nullable=True)
+    # last_name = db.Column(db.String(120), nullable=True)
+    # phone_number = db.Column(db.String(120), nullable=True)
+    # created_at = db.Column(db.DateTime, default=datetime.now)
+
+    # beyond
+    #
+    # email_auth_token = db.Column(db.Text, nullable=True)
+    # is_authenticated = db.Column(db.Boolean, default=False)
+
+    # relationships
+
+    timesheets = db.relationship("Timesheet", backref="user")
+
+    def __init__(self, username, password, email, timesheets):
+        self.username = username
+        self.password = password
+        self.email = email
+        self.timesheets = timesheets
 
     def __repr__(self):
-        return f"User('{self.username}', '{self.email}')"
+        return '<User %r>' % self.username
+
+class Timesheet(db.Model):
+
+    # required
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.DateTime, nullable=False)
+    # date_submitted = db.Column(db.DateTime, nullable=True, default=None)
+    clock_in_time = db.Column(db.DateTime, nullable=False)
+    clock_out_time = db.Column(db.DateTime, nullable=True)
+    # is_clocked_in = db.Column(db.Boolean, default=False)
+
+    def __init__(self, date, clock_in_time, clock_out_time):
+        self.date = date
+        self.clock_in_time = clock_in_time
+        self.clock_out_time = clock_out_time
+
+
+    user_id = db.Column(db.Integer(),
+                          db.ForeignKey("user.id"))
+
 
 if tableDoesNotExist(User.__tablename__):
     db.drop_all()
     db.create_all()
+
+# that is how we use it!
+# ts_1 = Timesheet(datetime.utcnow(), datetime.utcnow(), datetime.now())
+# user_1 = User("islomzhan", "tests", "the@database.comm", [ts_1])
+#
+# db.session.add_all([ts_1])
+# db.session.add(user_1)
+# db.session.commit()
+
+

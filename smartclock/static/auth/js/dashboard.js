8fcbb074fb7f9e0ee0c66b0d12135728e07b1fe8
Islomzhan Akhmedov <islomzhan@gmail.com>
2019-11-30 07:55:47 -0500

    at half way to finish dashboard

---
+++ smartclock/static/auth/js/dashboard.js
@@ -0,0 +1,7 @@
+function updateTime() {
+            $('#dayname').html(moment().format('dddd').toString());
+            $('#dateinfo').html(moment().format('MMMM D YYYY').toString());
+            $('#timeinfo').html(moment().format('h:mm:ss a').toString());
+            setTimeout(updateTime, 1000);
+        }
+updateTime();

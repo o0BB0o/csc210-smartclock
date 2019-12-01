b4708d1c31d8a6d4d971badb0e436682ef935f93
Islomzhan Akhmedov <islomzhan@gmail.com>
2019-12-01 06:14:14 -0500

    rest api works for get user and timesheet. One post method for timesheet also works.

--- smartclock/models.py
+++ smartclock/models.py
@@ -1,7 +1,8 @@
-from smartclock import db, login_manager
+from smartclock import db, ma, login_manager
 from flask_login import UserMixin
-from smartclock.functions import tableDoesNotExist, hash_password
+from smartclock.functions import tableDoesNotExist
 from datetime import datetime
+
 
 """
 Learn more here:
@@ -25,7 +26,7 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(120), unique=True, nullable=False)
     first_name = db.Column(db.String(120), nullable=False)
     last_name = db.Column(db.String(120), nullable=False)
-    created_at = db.Column(db.DateTime, default=datetime.now)
+    created_at = db.Column(db.DateTime, default=datetime.now, nullable=False)
 
     num_of_days_missing =  db.Column(db.Integer, default=0, nullable=False)
     num_of_days_left_early =  db.Column(db.Integer, default=0, nullable=False)
@@ -47,19 +48,34 @@ class User(db.Model, UserMixin):
     def __repr__(self):
         return '<User %r>' % self.username
 
+class UserSchema(ma.Schema):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'first_name', 'last_name', 'email', 'is_approved', 'created_at', 'num_of_days_missing', 'num_of_days_left_early', 'num_of_days_coming_late', 'is_admin', 'hourly_rate')
+
+user_schema = UserSchema(strict=True)
+users_schema = UserSchema(many=True, strict=True)
+
 
 class Timesheet(db.Model):
     # required
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.DateTime, nullable=False)
-    # date_submitted = db.Column(db.DateTime, nullable=True, default=None)
     clock_in_time = db.Column(db.DateTime, nullable=False)
     clock_out_time = db.Column(db.DateTime, nullable=True)
-
-    # is_clocked_in = db.Column(db.Boolean, default=False)
-
+    is_clocked_in = db.Column(db.Boolean, default=False, nullable=False)
     user_id = db.Column(db.Integer(),
                         db.ForeignKey("user.id"))
+
+class TimesheetSchema(ma.Schema):
+    class Meta:
+        model = Timesheet
+        fields = ('id', 'date', 'clock_in_time', 'clock_out_time', 'is_clocked_in', 'user_id')
+
+timesheet_schema = TimesheetSchema(strict=True)
+timesheets_schema = TimesheetSchema(many=True, strict=True)
+
+
 
 
 if tableDoesNotExist(User.__tablename__):
--- smartclock/routes.py
+++ smartclock/routes.py
@@ -1,9 +1,11 @@
 from smartclock import app, db
 from flask import render_template, redirect, url_for, flash, request, jsonify
 from smartclock.forms import RegistrationForm, LoginForm
-from smartclock.models import User
+from smartclock.models import User, Timesheet, user_schema, users_schema, timesheet_schema, timesheets_schema
 from smartclock.functions import check_password, hash_password
 from flask_login import login_user, logout_user, login_required, current_user
+from datetime import datetime
+
 
 
 @app.route("/")
@@ -95,3 +97,53 @@ def settings():
 @login_required
 def view():
     return render_template('auth/view.html', title='View Timesheets')
+
+
+
+"""
+    REST API Implementation
+"""
+# get all users
+@app.route('/api/v1/users', methods=['GET'])
+def get_users():
+    all_users = User.query.all()
+    result =  users_schema.dump(all_users)
+    return jsonify(result)
+
+# get a user by its username
+@app.route('/api/v1/users/<username>', methods=['GET'])
+def get_user(username):
+    user = User.query.filter_by(username=username).first()
+    if not user:
+        return jsonify({'message':'user does not exist'})
+    return user_schema.jsonify(user)
+
+# get all timesheets
+@app.route('/api/v1/timesheets', methods=['GET'])
+def get_timesheets():
+    all_timesheets = Timesheet.query.all()
+    result =  timesheets_schema.dump(all_timesheets)
+    return jsonify(result)
+
+
+# get a timesheet by its id
+@app.route('/api/v1/timesheets/<int:id>', methods=['GET'])
+def get_timesheet(id):
+    timesheet = Timesheet.query.get_or_404(id)
+    if not timesheet:
+        return jsonify({'message':'timesheet does not exist'})
+    return timesheet_schema.jsonify(timesheet)
+
+
+# post a timesheet by its id
+@app.route('/api/v1/timesheets/', methods=['POST'])
+def post_timesheet():
+    date = datetime.utcnow()
+    clock_in_time = datetime.utcnow()
+    clock_out_time = datetime.now()
+    is_clocked_in = request.json['is_clocked_in']
+    user_id = request.json['user_id']
+    timesheet = Timesheet(date = date, clock_in_time=clock_in_time, clock_out_time=clock_out_time, is_clocked_in=is_clocked_in, user_id=user_id)
+    db.session.add(timesheet)
+    db.session.commit()
+    return timesheet_schema.jsonify(timesheet)

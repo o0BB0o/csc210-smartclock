e96d26040adc5402c345dc20b106203703f3e928
Islomzhan Akhmedov <islomzhan@gmail.com>
2019-12-05 14:52:41 -0500

    patch is working!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!....

--- smartclock/static/auth/js/user.js
+++ smartclock/static/auth/js/user.js
@@ -15,33 +15,48 @@ function currentFile() {
 username = currentFile();
 
 $('.delete-me').each(function () {
-    $(this).on('click', function () {
-        if (confirm('Do you really want to delete this account?')) {
-            let url = window.origin+"/api/v1/user/"+username;
-            $.ajax({
-                "url": url,
-                "type": "DELETE",
-            }).done(function (){
-                 window.location.replace(window.origin+"/dashboard");
-            });
-        } else {
-            alert('Alright!');
-        }
-    });
+	$(this).on('click', function () {
+		if (confirm('Do you really want to delete this account?')) {
+			let url = window.origin + "/api/v1/user/" + username;
+			$.ajax({
+				"url": url,
+				"type": "DELETE",
+			}).done(function () {
+				window.location.replace(window.origin + "/dashboard");
+			});
+		} else {
+			alert('Alright!');
+		}
+	});
 });
 
-$('.approve-me').on('click',  function () {
-        if (confirm('Do you want to approve this account?')) {
-            let url = window.origin+"/api/v1/users/approve/"+username;
-            $.ajax({
-                "url": url,
-                "type": "PATCH",
-                "data": {"is_approved":true},
-            }).done(function (){
-                 window.location.replace(window.origin+"/dashboard");
-            });
+$('.approve-me').on('click', function () {
+	if (confirm('Do you want to approve this account?')) {
+		let url = window.origin + "/api/v1/users/approve/" + username;
+		casted_url = String(url); // I don't know what am I doing...
+		data_json = {
+					"is_approved": true
+				}
+		$.ajax({
+				"url": casted_url,
+				"data": JSON.stringify(data_json),
+				"dataType": "json",
+				"contentType": "application/json",
+				"type": "PATCH"
+			})
+			.done(function () {
+				window.location.replace(window.origin + "/dashboard");
+			})
+			.fail(function (xhr, status, description) {
+				alert("There was a problem handling this request");
+				console.log("Error: " + description);
+				console.log("Status: + status");
+			})
+			.always(function (xhr, status) {
+				console.log("request completed with status code: " + status);
+			});
 
-        } else {
-            alert('Alright!');
-        }
+	} else {
+		alert('Alright!');
+	}
 });
